a148a64bba200eef679e44d342a895eb737f3bd4
ovie umukoro <ovie.u.umukoro@gmail.com>
2020-02-27 13:16:33 -0500

    Check if list has cycle

--- datastructs/fastSlowPointer.py
+++ datastructs/fastSlowPointer.py
@@ -12,6 +12,30 @@ def has_cycle(head: Node) -> bool:
             return True
     return False
 
+def get_cycle_start(head: Node) -> Node:
+    fast, slow = head, head
+    while fast is not None and fast.next is not None:
+        fast = fast.next.next
+        slow = slow.next
+        if fast == slow:
+            return fast
+    return None
+
+def get_cycle_len(head: Node) -> int:
+    p = head
+    len = 0
+    while True:
+        p = p.next
+        len += 1
+        if p == head:
+            break
+    return len
+
+def calculate_cycle_length(head: Node) -> int:
+    cycle_meet_point = get_cycle_start(head)
+    cycle_len = get_cycle_len(cycle_meet_point)
+    return cycle_len
+
 def main():
     head = Node(1)
     head.next = Node(2)
@@ -26,6 +50,8 @@ def main():
     
     head.next.next.next.next.next.next = head.next.next.next
     print("LinkedList has cycle: " + str(has_cycle(head)))
+    
+    print("cycle len of linkedlist is: " + f'{calculate_cycle_length(head)}')
 
 if __name__ == "__main__":
     main()
